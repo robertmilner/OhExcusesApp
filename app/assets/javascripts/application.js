1d9a4d06403218bd1d0e8dfc7eee457b8045ce2b
Robert Milner <dev@rbrtmlnr.com>
2012-03-23 13:07:10 -0500

    AJAX needs work. Turned off for now.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,54 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-transition
 //= require bootstrap-tooltip
 //= require bootstrap-carousel
 //= require_tree .
-
-// need to get button working first
-// enable and disable button
-// favorite_btn
-$(document).ready(function($) {
-
-	$('.carousel').carousel({
-		interval: 10000
-	});
-	$('.carousel-previous-button').click(function() {
-		$('.carousel').carousel('prev');
-	});
-	$('.carousel-forward-button').click(function() {
-		$('.carousel').carousel('next');
-	});
-
-//     // $("#disableBtn").click(function() {
-//     //     $("#clearBtn").attr("disabled", "disabled");
-//     // }); // click()
-//     // $("#enableBtn").click(function() {
-//     //     $("#clearBtn").removeAttr("disabled");
-//     // }); // click()
-// 
-// 	// VERTICALLY ALIGN FUNCTION
-// 	// $.fn.vertAlign = function() {
-// 	// 	return this.each(function(i){
-// 	// 	var ah = $(this).height();
-// 	// 	var ph = $(this).parent().height();
-// 	// 	var mh = Math.ceil((ph-ah) / 2);
-// 	// 	$(this).css('margin-top', mh);
-// 	// 	});
-// 	// };
-// 	
-// 	// $('.label').vertAlign();
-
-}); // ready()
---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,34 @@
+// if (history && history.pushState) {
+//   $(function() {
+//     $("footer ul li a, .location ul li a, .tags ul li a").live("click", function(e) {
+//       $.getScript(this.href);
+//       history.pushState(null, document.title, this.href);
+//       e.preventDefault();
+//     });
+//     $(".search input").keyup(function() {
+//       $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+//       history.replaceState(null, document.title, $("#products_search").attr("action") + "?" + $("#products_search").serialize());
+//     });
+//     $(window).bind("popstate", function() {
+//       $.getScript(location.href);
+//     });
+//   });
+// };
+
+$(document).ready(function($) {
+
+
+	// Pages - Carousel Partial
+	$('.carousel').carousel({
+		interval: 10000
+	});
+	$('.carousel-previous-button').click(function() {
+		$('.carousel').carousel('prev');
+	});
+	$('.carousel-forward-button').click(function() {
+		$('.carousel').carousel('next');
+	});
+	// end
+
+
+});
